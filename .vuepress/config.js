09d1176ebff89cd0d670971a56d056ba2bb3df36
叶远达 <YEYUANDA995@pingan.com.cn>
2019-12-14 20:25:42 +0800

    init

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     lang: "zh-CN",
     title: "JOJO的奇妙笔记",
     description: "专注前端方向与可视化领域的博客",
-    base: '/', // 这是部署到github相关的配置
+    base: '/blog/', // 这是部署到github相关的配置
     head: headConf,
     markdown: mdConf,
     themeConfig: themeConf,
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ home: true
 heroImage: "./edit-tools.png"
 heroText: JOJO的奇妙笔记
 tagline: 专注前端
-actionText: 快速上手 →
+actionText: start →
 actionLink: /guide/
 footer: MIT Licensed | Copyright © 2019 JOJO LIN
 ---
