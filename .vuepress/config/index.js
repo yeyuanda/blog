be0376ef512c3b0ca393cac85fe2021402e65a2d
叶远达 <YEYUANDA995@pingan.com.cn>
2019-12-14 20:05:31 +0800

    int

---
+++ .vuepress/config/index.js
@@ -0,0 +1,9 @@
+const mdConf = require("./markdown");
+const themeConf = require("./themeConf");
+const headConf = require("./head");
+
+module.exports = {
+    mdConf,
+    themeConf,
+    headConf
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+---
+home: true
+heroImage: "./edit-tools.png"
+heroText: JOJO的奇妙笔记
+tagline: 专注前端
+actionText: 快速上手 →
+actionLink: /guide/
+footer: MIT Licensed | Copyright © 2019 JOJO LIN
+---
